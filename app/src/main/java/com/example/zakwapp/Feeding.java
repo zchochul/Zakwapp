e4b2e1e9510bceabd9b57576dd84917dbf8bd0cd
Zuzanna Chochulska <zuzanna.chochulska.stud@pw.edu.pl>
2023-05-09 17:19:07 +0200

    pierwsze activity

---
+++ app/src/main/java/com/example/zakwapp/Feeding.java
@@ -0,0 +1,14 @@
+package com.example.zakwapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Feeding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feeding);
+    }
+}
--- app/src/main/java/com/example/zakwapp/MainActivity.java
+++ app/src/main/java/com/example/zakwapp/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.zakwapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button_feeding;
+    Button button_is_dead;
+    Button button_movies;
+    Button button_calendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("d");
+        button_feeding = findViewById(R.id.Feed_button);
+        button_is_dead = findViewById(R.id.Is_Dead_Button);
+        button_movies = findViewById(R.id.Movies_Button);
+        button_calendar = findViewById(R.id.Calendar_Button);
+
+        button_feeding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentFeed = new Intent(getBaseContext(),Feeding.class);
+                startActivity(intentFeed);
+            }
+        });
     }
 }
