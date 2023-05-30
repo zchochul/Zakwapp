9558b323b9b199872eef38fb5efd1847d4e6def8
Admin <Admin@DESKTOP-IHOACRJ>
2023-05-30 17:52:13 +0200

    filmik gra

--- app/src/main/java/com/example/zakwapp/Video.java
+++ app/src/main/java/com/example/zakwapp/Video.java
@@ -6,20 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 public class Video extends AppCompatActivity {
 
         Button goBack;
+        VideoView video;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
         goBack = findViewById(R.id.buttonGoBack);
+        video = findViewById(R.id.videoView);
+        String path = "android.resource://"+getPackageName()+"/"+R.raw.howtochleb;
+        video.setVideoPath(path);
+        video.start();
         goBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                video.stopPlayback();
                 Intent intentMain = new Intent(getBaseContext(),MainActivity.class);
                 startActivity(intentMain);
             }
